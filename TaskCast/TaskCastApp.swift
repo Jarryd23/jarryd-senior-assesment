7c403d88e007355f8d77f08a3668cb59c051024e
Jarryd23 <jarrydbaillie23@gmail.com>
2025-07-05 20:45:46 +0200

    initial commit

---
+++ TaskCast/TaskCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskCastApp.swift
+//  TaskCast
+//
+//  Created by Jarryd Baillie on 2025/07/05.
+//
+
+import SwiftUI
+
+@main
+struct TaskCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
