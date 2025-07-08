58ba2e994af23241d3e04f9a85b3255f7a88d26b
Jarryd23 <jarrydbaillie23@gmail.com>
2025-07-08 22:45:57 +0200

    Move map selection view

--- TaskCast/Generated/NeedleGenerated.swift
+++ TaskCast/Generated/NeedleGenerated.swift
@@ -30,6 +30,41 @@ private class TaskComponentDependenciesd53b7bef8c827695e7b5Provider: TaskCompone
 private func factory300b36c36b94ecb3c2e5b3a8f24c1d289f2c0f2e(_ component: NeedleFoundation.Scope) -> AnyObject {
     return TaskComponentDependenciesd53b7bef8c827695e7b5Provider(rootComponent: parent1(component) as! RootComponent)
 }
+private class WeatherComponentDependenciescb93fc78d7cc2ccc8ba5Provider: WeatherComponentDependencies {
+    var realmAdapter: RealmAdapter {
+        return rootComponent.realmAdapter
+    }
+    var networkClient: NetworkClient {
+        return rootComponent.networkClient
+    }
+    private let rootComponent: RootComponent
+    init(rootComponent: RootComponent) {
+        self.rootComponent = rootComponent
+    }
+}
+/// ^->RootComponent->WeatherComponent
+private func factory9f1603ad9e8db7410ca4b3a8f24c1d289f2c0f2e(_ component: NeedleFoundation.Scope) -> AnyObject {
+    return WeatherComponentDependenciescb93fc78d7cc2ccc8ba5Provider(rootComponent: parent1(component) as! RootComponent)
+}
+private class OnboardingComponentDependenciesc2e150944dc3c9e77b26Provider: OnboardingComponentDependencies {
+    var realmAdapter: RealmAdapter {
+        return rootComponent.realmAdapter
+    }
+    var networkClient: NetworkClient {
+        return rootComponent.networkClient
+    }
+    var weatherComponent: WeatherComponent {
+        return rootComponent.weatherComponent
+    }
+    private let rootComponent: RootComponent
+    init(rootComponent: RootComponent) {
+        self.rootComponent = rootComponent
+    }
+}
+/// ^->RootComponent->OnboardingComponent
+private func factory269b0df6e85393f6e468b3a8f24c1d289f2c0f2e(_ component: NeedleFoundation.Scope) -> AnyObject {
+    return OnboardingComponentDependenciesc2e150944dc3c9e77b26Provider(rootComponent: parent1(component) as! RootComponent)
+}
 
 #else
 extension TasksComponent: NeedleFoundation.Registration {
@@ -37,11 +72,30 @@ extension TasksComponent: NeedleFoundation.Registration {
         keyPathToName[\TaskComponentDependencies.realmAdapter] = "realmAdapter-RealmAdapter"
     }
 }
+extension WeatherComponent: NeedleFoundation.Registration {
+    public func registerItems() {
+        keyPathToName[\WeatherComponentDependencies.realmAdapter] = "realmAdapter-RealmAdapter"
+        keyPathToName[\WeatherComponentDependencies.networkClient] = "networkClient-NetworkClient"
+    }
+}
+extension OnboardingComponent: NeedleFoundation.Registration {
+    public func registerItems() {
+        keyPathToName[\OnboardingComponentDependencies.realmAdapter] = "realmAdapter-RealmAdapter"
+        keyPathToName[\OnboardingComponentDependencies.networkClient] = "networkClient-NetworkClient"
+        keyPathToName[\OnboardingComponentDependencies.weatherComponent] = "weatherComponent-WeatherComponent"
+    }
+}
 extension RootComponent: NeedleFoundation.Registration {
     public func registerItems() {
 
         localTable["realmAdapter-RealmAdapter"] = { [unowned self] in self.realmAdapter as Any }
+        localTable["networkClient-NetworkClient"] = { [unowned self] in self.networkClient as Any }
         localTable["tasksComponent-TasksComponent"] = { [unowned self] in self.tasksComponent as Any }
+        localTable["weatherComponent-WeatherComponent"] = { [unowned self] in self.weatherComponent as Any }
+        localTable["onboardingComponent-OnboardingComponent"] = { [unowned self] in self.onboardingComponent as Any }
+        localTable["rootView-some View"] = { [unowned self] in self.rootView as Any }
+        localTable["mapSelectionView-some View"] = { [unowned self] in self.mapSelectionView as Any }
+        localTable["dashboardViewModel-some DashboardViewModel"] = { [unowned self] in self.dashboardViewModel as Any }
     }
 }
 
@@ -61,6 +115,8 @@ private func registerProviderFactory(_ componentPath: String, _ factory: @escapi
 
 @inline(never) private func register1() {
     registerProviderFactory("^->RootComponent->TasksComponent", factory300b36c36b94ecb3c2e5b3a8f24c1d289f2c0f2e)
+    registerProviderFactory("^->RootComponent->WeatherComponent", factory9f1603ad9e8db7410ca4b3a8f24c1d289f2c0f2e)
+    registerProviderFactory("^->RootComponent->OnboardingComponent", factory269b0df6e85393f6e468b3a8f24c1d289f2c0f2e)
     registerProviderFactory("^->RootComponent", factoryEmptyDependencyProvider)
 }
 #endif
