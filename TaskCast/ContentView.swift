aae6edd8058b4f13bb7023b9cd349fcde6d9ffa3
Jarryd23 <jarrydbaillie23@gmail.com>
2025-07-08 22:33:33 +0200

    Update main content view for root view of app

--- TaskCast/ContentView.swift
+++ TaskCast/ContentView.swift
@@ -1,22 +1,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("hasFinishedOnboarding") var hasFinishedOnboarding: Bool = false
     var body: some View {
-        TabView {
-            DashboardView()
-                
+        if hasFinishedOnboarding {
+            TabView {
+                NavigationStack {
+                    DependecyContainer.shared.rootComponent.rootView
+                }
                 .tabItem {
                     Label("Today",
                           systemImage: "house")
                 }
+                .toolbarBackground(.visible, for: .tabBar)
+                .toolbarBackground(.ultraThinMaterial, for: .tabBar)
                 
-            NavigationStack {
-                RootComponent().tasksComponent.rootView
-                    .tabItem {
-                        Label("All Tasks",
-                              systemImage: "list.bullet")
-                    }
+                
+                NavigationStack {
+                    DependecyContainer.shared.rootComponent.tasksComponent.rootView
+                }
+                .tabItem {
+                    Label("All Tasks",
+                          systemImage: "list.bullet")
+                }
             }
+            .tint(.orange)
+        } else {
+            OnboardingView(viewmodel: DependecyContainer.shared.rootComponent.onboardingComponent.onboardingViewModel)
+        }
+    }
+}
+
+
+import SwiftUI
+
+struct LocationUpdateView: View {
+    @State var showMapSelectionView: Bool = false
+    var body: some View {
+        TaskCastPrimaryButton(buttonText: "Change Location", action: {
+            showMapSelectionView.toggle()
+        })
+        .sheet(isPresented: $showMapSelectionView) {
+            RootComponent().mapSelectionView
         }
     }
 }
